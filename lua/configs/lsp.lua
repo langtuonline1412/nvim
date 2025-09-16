c0c99b8be254da4595d6ddb47ef7137fe6d7719d
dai <langtuonline1412@gmail.com>
2025-09-16 07:26:29 +0700

    Them file cau hinh lualine, them lsp cho typescript va javascript

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -1,56 +1,62 @@
-
 local lspconfig = require("lspconfig")
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- Python
-lspconfig.pyright.setup {
-  capabilities = capabilities,
-}
+lspconfig.pyright.setup({
+	capabilities = capabilities,
+})
 
 -- C++
-lspconfig.clangd.setup {
-  capabilities = capabilities,
-}
+lspconfig.clangd.setup({
+	capabilities = capabilities,
+})
 
 -- Lua
-lspconfig.lua_ls.setup {
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = { globals = { "vim" } }
-    }
-  }
-}
+lspconfig.lua_ls.setup({
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = { globals = { "vim" } },
+		},
+	},
+})
+
+-- TypeScript
+lspconfig.ts_ls.setup({
+	on_attach = on_attach,
+	filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+	cmd = { "typescript-language-server", "--stdio" },
+})
 
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "pyright", "clangd", "jdtls" },
-        automatic_installation = true,
-      })
-    end,
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls", "pyright", "clangd", "jdtls" },
+				automatic_installation = true,
+			})
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local servers = { "lua_ls", "tsserver", "pyright", "clangd", "jdtls" }
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local servers = { "lua_ls", "ts_ls", "pyright", "clangd", "jdtls" }
 
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({})
-      end
-    end,
-  },
+			for _, server in ipairs(servers) do
+				lspconfig[server].setup({})
+			end
+		end,
+	},
 }
